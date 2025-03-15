5bcef2f6ab8fec59c22ab901255bd2547683e093
Chetanveer <gobinashnil@gmail.com>
2025-03-15 19:24:29 +0000

    added startup script

---
+++ on-start.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+# Wait for 5 seconds
+sleep 5
+
+export PATH="$HOME/miniconda3/bin:$PATH"
+conda init
+
+
+
