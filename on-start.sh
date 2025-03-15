559b43b87109edd9ef56f00c5cd73abf02989e51
Chetanveer <gobinashnil@gmail.com>
2025-03-15 21:56:27 +0000

    added keygen to on start

--- on-start.sh
+++ on-start.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
@@ -8,6 +10,14 @@ sleep 5
 
 export PATH="$HOME/miniconda3/bin:$PATH"
 conda init
+
+
+# Wait for 5 seconds
+sleep 5
+## Keygen 
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+
 
 
 
