7b63a33fcbfbee10a42081d9f44b6c6218cba799
Chetanveer <gobinashnil@gmail.com>
2025-03-16 11:37:50 +0000

    launching several experiments in parallel

--- experimentlauncher.sh
+++ experimentlauncher.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
 
+## Experiment 1
+python trainer.py --batch_size 5 --epochs 11 &
 
-python trainer.py --batch_size 5 --epochs 11
+## Experiment 2
+python trainer.py --batch_size 5 --epochs 14 &
+
+## Experiment 3
+python trainer.py --batch_size 5 --epochs 16 &
+
+
+wait 
+
+echo "all experiments are done"
