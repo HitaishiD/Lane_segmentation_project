7f0365890336bc47c6bfef1f451f0898ece7afcc
HitaishiD <hitaishi.dh@gmail.com>
2025-03-18 13:37:21 -0400

    added docstring and comments

--- classical_method/inference.py
+++ classical_method/inference.py
@@ -2,7 +2,22 @@ from classical_method import ClassicalMethod
 
 class Inference:
     def predict(self, image_path):
+        """
+        Predicts the lane segment in a given image
+
+        Parameters:
+        image_path (str): The file path of the image for which lane detection is performed
+
+        Returns:
+        numpy.ndarray: The processed image with the detected lane segment
+
+         """
+        
+        # Create an instance of the ClassicalMethod class
         classical_method = ClassicalMethod(image_path)
+
+        # Call the process method to perform lane detection and get the output
         output = classical_method.process()
+        
         return output
 
