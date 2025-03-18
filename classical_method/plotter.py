bef913d9a800fbc855ca08f974e5e9ed439b4282
HitaishiD <hitaishi.dh@gmail.com>
2025-03-18 13:42:44 -0400

    added docstring and comments

--- classical_method/plotter.py
+++ classical_method/plotter.py
@@ -5,13 +5,38 @@ import matplotlib.pyplot as plt
 
 class Plotter:
     def plot_from_path(self, image_path):
+        """
+        Plots an image from its file path
+        
+        Parameters:
+        image_path (str): The file path of the image which is plotted
+
+        Returns:
+        None
+
+        """
+        # Open an image using the PIL library and convert it to RGB
         image = Image.open(image_path).convert("RGB")
+
+        # Convert PIL image into numpy.ndarray
         image_array = np.array(image)
+
+        # Plot and show image
         plt.imshow(image_array)
         plt.axis('off')
         plt.show()
 
     def plot_array(self, array):
+        """
+        Plots an image from its numpy.ndarray
+        
+        Parameters:
+        numpy.ndarray: The numpy.ndarray of the image which is plotted
+
+        Returns:
+        None
+        """
+        # Plot and show image
         plt.imshow(array)
         plt.axis('off')
         plt.show()
