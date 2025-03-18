1994a82c881d2df6a15cdff119fa35a92916bf79
HitaishiD <hitaishi.dh@gmail.com>
2025-03-18 13:44:08 -0400

    testing classical method

--- classical_method/test_classical_method.py
+++ classical_method/test_classical_method.py
@@ -1,11 +1,12 @@
 from classical_method import ClassicalMethod
 import matplotlib.pyplot as plt
 
-image_path = r"E:\data_semantics\training\image_2\000104_10.png"
+image_path = r"E:\data_semantics\training\image_2\000105_10.png"
+
 classical_method = ClassicalMethod(image_path)
 output = classical_method.process()
 
 print(classical_method.lines)
-# plt.imshow(output)
-# plt.axis('off')
-# plt.show()
+plt.imshow(output)
+plt.axis('off')
+plt.show()
