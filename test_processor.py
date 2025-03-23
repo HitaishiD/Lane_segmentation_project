6b2b39dbccf3953c46028dbbcf7097a6a5274346
HitaishiD <hitaishi.dh@gmail.com>
2025-03-23 18:43:15 +0000

    changed output folder name to eliminate mask folder renaming step

--- test_processor.py
+++ test_processor.py
@@ -4,7 +4,7 @@ from color_map import color_map
 # Test Preprocessor class
 
 mask_folder = '/home/ubuntu/computer-vision/computer-vision/training/semantic_rgb'
-output_mask_folder = '/home/ubuntu/computer-vision/computer-vision/testing_preprocessed_masks'
+output_mask_folder = '/home/ubuntu/computer-vision/computer-vision/preprocessed_masks'
 
 preprocessor = Processor()
 preprocessor.convert_rgb_to_mask(mask_folder, output_mask_folder, color_map)
