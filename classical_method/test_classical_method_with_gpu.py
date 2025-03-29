c67bb9ca8c1147f27d6f29738832c36ae9798043
HitaishiD <hitaishi.dh@gmail.com>
2025-03-29 16:39:13 -0400

    removed unnecessary files for classical method

--- classical_method/test_classical_method_with_gpu.py
+++
@@ -1,9 +0,0 @@
-from classical_method_with_gpu import ClassicalMethod
-import matplotlib.pyplot as plt
-
-image_path = r"/home/ubuntu/computer-vision/training/image_2/000002_10.png"
-classical_method = ClassicalMethod(image_path)
-output = classical_method.process()
-
-plt.imshow(output)
-plt.show()
