70a53c00abcd4c14d29008491c5532ec553cec15
HitaishiD <hitaishi.dh@gmail.com>
2025-03-31 16:25:27 -0400

    added plot for iou for classical method

--- plotter.py
+++ plotter.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
 
+# ***************** Modern method *****************
 # Path to CSV file containing training losses
 csv_file = "experiments/bs8_lr0.000146_epochs31/training_losses.csv"
 # Path at which to save the plot as an image
@@ -18,22 +20,33 @@ val_losses = loss_df['Validation Loss'] if 'Validation Loss' in loss_df.columns 
 val_iou = loss_df['Validation IoU'] if 'Validation IoU' in loss_df.columns else None
 
 # Plot the training loss
-plt.figure(figsize=(10, 6))
-plt.plot(epochs, train_losses, label="Training Loss", color='b', marker='o')
+ax[0].plot(epochs, train_losses, label="Training Loss", color='b', marker='o')
 
 # Plot the validation loss if it exists
 if val_losses is not None:
-    plt.plot(epochs, val_losses, label="Validation Loss", color='r', marker='x')
+    ax[0].plot(epochs, val_losses, label="Validation Loss", color='r', marker='x')
 
 # Plot the validation IoU if it exists
 if val_iou is not None:
-    plt.plot(epochs, val_iou, label="Validation IoU", color='g', marker='v')
+    ax[0].plot(epochs, val_iou, label="Validation IoU", color='g', marker='v')
 
 # Add labels and title
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Loss Over Epochs')
-plt.legend()
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('Loss')
+ax[0].set_title('Loss Over Epochs for Modern Method')
+ax[0].legend()
+
+# ***************** Classical method *****************
+csv_file2 = "iou.csv"
+iou_df = pd.read_csv(csv_file2)
+image_num = iou_df['Image Number']
+iou = iou_df['IoU']
+
+ax[1].scatter(image_num, iou, color='g', marker='v')
+ax[1].set_xlabel('Image Number')
+ax[1].set_ylabel('IoU')
+ax[1].set_title('IoU for all images for Classical Method')
+
 
 # Show figure
 plt.show()
