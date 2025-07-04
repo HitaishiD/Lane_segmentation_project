3c3fb4f09edb69bb7ac47d643581b1026bf8a1c8
HitaishiD <hitaishi.dh@gmail.com>
2025-07-04 15:29:18 +0400

    added save output mask

--- framework/test_inference.py
+++ framework/test_inference.py
@@ -1,5 +1,6 @@
 # Import required libraries
 import torch
+from PIL import Image
 
 # Import custom classes
 from framework.processor import Processor
@@ -11,14 +12,19 @@ from framework.model import DeepLabV3Plus
 NUM_CLASSES = 13
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-MODEL_PATH = "/home/ubuntu/computer-vision/computer-vision/experiments/modelweights"
+MODEL_PATH = "/home/hitaishi/lane-segmentation/Lane_segmentation_project/experiments/final_model.pth"
 model = DeepLabV3Plus(NUM_CLASSES)
 model.load_state_dict(torch.load(MODEL_PATH, map_location = DEVICE))
 model.to(DEVICE)
  
 inference = Inference(model, color_map, DEVICE)
-image_path = "/home/ubuntu/computer-vision/computer-vision/testing/image_2/000160_10.png"
-inference.predict_from_image_path(image_path)
+image_path = "/media/hitaishi/9961-4D68/data_semantics/training/image_2/000041_10.png"
  
-test = inference.predict_from_image_path(image_path)
-print(type(test))
+output_mask = inference.predict_from_image_path(image_path)
+
+original_image = Image.open(image_path)
+original_size = original_image.size
+
+output_mask_im = Image.fromarray(output_mask)
+output_mask_im = output_mask_im.resize(original_size)
+output_mask_im.save('output_mask.png')
