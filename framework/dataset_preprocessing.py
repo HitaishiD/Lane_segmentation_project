e3705fcddb24c37c8500a368aa53aa6dd196e04d
HitaishiD <hitaishi.dh@gmail.com>
2025-07-04 06:09:39 +0000

    modified directory paths

--- framework/dataset_preprocessing.py
+++ framework/dataset_preprocessing.py
@@ -3,8 +3,8 @@ from framework.processor import Processor
 from framework.color_map import color_map
 
 # Define folders containing input masks and output masks
-mask_folder = '/home/ubuntu/computer-vision/computer-vision/training/semantic_rgb'
-output_mask_folder = '/home/ubuntu/computer-vision/computer-vision/preprocessed_masks'
+mask_folder = '/root/Lane_segmentation_project/dataset/training/semantic_rgb'
+output_mask_folder = '/root/Lane_segmentation_project/dataset/training/preprocessed_masks'
 
 # Preprocess input masks according to color map
 preprocessor = Processor()
